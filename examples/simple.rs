af7b7d8ffdc4ffde623e2b48f293c52c08007562
Federico Maria Morrone <contact@morrone.dev>
2024-07-20 12:11:03 +0200

    Allow for custom open and close implementation and decouple stream from main struct

--- examples/simple.rs
+++ examples/simple.rs
@@ -3,13 +3,25 @@ use std::ffi::CString;
 use anyhow::Result as AnyResult;
 use colpetto::Libinput;
 use futures_util::TryStreamExt;
+use rustix::{
+    fd::{FromRawFd, IntoRawFd, OwnedFd},
+    fs::{open, Mode, OFlags},
+};
 
 #[tokio::main]
 async fn main() -> AnyResult<()> {
-    let mut libinput = Libinput::new()?;
+    let libinput = Libinput::new(
+        |path, flags| match open(path, OFlags::from_bits_retain(flags as u32), Mode::empty()) {
+            Ok(fd) => fd.into_raw_fd(),
+            Err(errno) => errno.raw_os_error(),
+        },
+        |fd| drop(unsafe { OwnedFd::from_raw_fd(fd) }),
+    )?;
     libinput.assign_seat(CString::new("seat0").unwrap().as_c_str())?;
 
-    while let Some(event) = libinput.try_next().await? {
+    let mut stream = libinput.event_stream()?;
+
+    while let Some(event) = stream.try_next().await? {
         dbg!(event);
     }
 
