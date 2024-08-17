3aeba7c27d49e6f738fc70d3d10bdd4cf5028f18
Matilde Morrone <contact@morrone.dev>
2024-08-17 19:38:30 +0200

    Relax libinput version requirement

--- build.rs
+++ build.rs
@@ -1,6 +1,6 @@
 fn main() {
     pkg_config::Config::new()
-        .atleast_version("1.26.1")
+        .atleast_version("1.26")
         .probe("libinput")
         .expect("Failed to link to libinput");
 }
