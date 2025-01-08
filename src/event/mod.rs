33262078b0215496d6815c9456d119e9235f5838
Matilde Morrone <contact@morrone.dev>
2025-01-08 20:46:11 +0100

    Map keyboard event

--- src/event/mod.rs
+++ src/event/mod.rs
@@ -26,7 +26,49 @@ pub enum KeyboardEvent {
 
 #[derive(Debug)]
 pub struct KeyboardEventKey {
-    // raw: *mut sys::libinput_event_keyboard,
+    raw: *mut sys::libinput_event_keyboard,
+}
+
+pub enum KeyState {
+    Released,
+    Pressed,
+}
+
+impl std::fmt::Display for KeyState {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            KeyState::Released => write!(f, "released"),
+            KeyState::Pressed => write!(f, "pressed"),
+        }
+    }
+}
+
+impl KeyState {
+    pub const fn from_raw(raw: sys::libinput_key_state) -> Self {
+        match raw {
+            sys::libinput_key_state::LIBINPUT_KEY_STATE_RELEASED => Self::Released,
+            sys::libinput_key_state::LIBINPUT_KEY_STATE_PRESSED => Self::Pressed,
+            _ => panic!("libinput returned an invalid keystate"), // FIXME: I dont think panicking is a good idea. Maybe we could return an option?
+        }
+    }
+}
+
+impl KeyboardEventKey {
+    pub fn key(&self) -> u32 {
+        unsafe { sys::libinput_event_keyboard_get_key(self.raw) }
+    }
+
+    pub fn key_state(&self) -> KeyState {
+        KeyState::from_raw(unsafe { sys::libinput_event_keyboard_get_key_state(self.raw) })
+    }
+
+    pub fn time(&self) -> u32 {
+        unsafe { sys::libinput_event_keyboard_get_time(self.raw) }
+    }
+
+    pub fn time_usec(&self) -> u64 {
+        unsafe { sys::libinput_event_keyboard_get_time_usec(self.raw) }
+    }
 }
 
 #[derive(Debug)]
@@ -96,10 +138,8 @@ impl Event {
                 Event::Device(DeviceEvent::Removed)
             }
             sys::libinput_event_type::LIBINPUT_EVENT_KEYBOARD_KEY => {
-                // let keyboard_event = unsafe { sys::libinput_event_get_keyboard_event(raw) };
-
                 Event::Keyboard(KeyboardEvent::Key(KeyboardEventKey {
-                    // raw: keyboard_event,
+                    raw: unsafe { sys::libinput_event_get_keyboard_event(raw) },
                 }))
             }
             sys::libinput_event_type::LIBINPUT_EVENT_POINTER_MOTION => {
@@ -188,8 +228,6 @@ impl Event {
             }
             _ => Event::Unknown,
         };
-
-        unsafe { sys::libinput_event_destroy(raw) };
 
         event
     }
