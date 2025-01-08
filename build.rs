fa9873b61673330ec37167ab39b552e349bc41b0
Matilde Morrone <contact@morrone.dev>
2025-01-08 17:25:23 +0100

    Bump libinput version requirement

--- build.rs
+++ build.rs
@@ -1,6 +1,6 @@
 fn main() {
     pkg_config::Config::new()
-        .atleast_version("1.26")
+        .atleast_version("1.27")
         .probe("libinput")
         .expect("Failed to link to libinput");
 }
