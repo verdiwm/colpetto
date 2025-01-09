165223b05551f2ac3f4ba863b4ec738e9b6641cd
Federico Maria Morrone <contact@morrone.dev>
2025-01-09 19:07:31 +0100

    Add getter for device name

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,7 +1,10 @@
 use std::ffi::CString;
 
 use anyhow::Result as AnyResult;
-use colpetto::{event::KeyboardEvent, Event, Libinput};
+use colpetto::{
+    event::{AsEvent, KeyboardEvent},
+    Event, Libinput,
+};
 use futures_util::TryStreamExt;
 use rustix::{
     fd::{FromRawFd, IntoRawFd, OwnedFd},
@@ -28,9 +31,11 @@ async fn main() -> AnyResult<()> {
 
     while let Some(event) = stream.try_next().await? {
         if let Event::Keyboard(KeyboardEvent::Key(event_key)) = event {
+            let device = event_key.device();
+            let device_name = device.name().to_string_lossy();
             let state = event_key.key_state();
             let key = event_key.key();
-            println!("Key \"{key}\" {state}")
+            println!("Key \"{key}\" {state} on \"{device_name}\"")
         }
     }
 
--- src/device.rs
+++ src/device.rs
@@ -1,3 +1,5 @@
+use std::ffi::CStr;
+
 use crate::sys;
 
 pub struct Device {
@@ -7,5 +9,9 @@ pub struct Device {
 impl Device {
     pub unsafe fn from_raw(raw: *mut sys::libinput_device) -> Self {
         Self { raw }
+    }
+
+    pub fn name(&self) -> &CStr {
+        unsafe { CStr::from_ptr(sys::libinput_device_get_name(self.raw)) }
     }
 }
