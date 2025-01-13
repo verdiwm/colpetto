45a04873ec8efa16f4b9bcc965cf94458d0bc69c
Matilde Morrone <contact@morrone.dev>
2025-01-13 20:26:15 +0100

    Significantly reduce boilerplate with a better define_events macro

--- src/event/touch.rs
+++ src/event/touch.rs
@@ -2,24 +2,14 @@ use crate::sys::{
     libinput_event_get_touch_event, libinput_event_touch, libinput_event_touch_get_base_event,
 };
 
-use super::define_events;
-
-#[derive(Debug)]
-pub enum TouchEvent {
-    Down(TouchDownEvent),
-    Up(TouchUpEvent),
-    Motion(TouchMotionEvent),
-    Cancel(TouchCancelEvent),
-    Frame(TouchFrameEvent),
-}
-
-define_events!(
+super::define_events!(
+    Touch,
     libinput_event_touch,
     libinput_event_touch_get_base_event,
     libinput_event_get_touch_event,
-    TouchDownEvent,
-    TouchUpEvent,
-    TouchMotionEvent,
-    TouchCancelEvent,
-    TouchFrameEvent,
+    Down,
+    Up,
+    Motion,
+    Cancel,
+    Frame,
 );
