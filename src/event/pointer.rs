1397e64494d3f586e5a031f5177b2e0f5161316c
Federico Maria Morrone <contact@morrone.dev>
2025-01-13 21:01:10 +0100

    Implement some pointer absolute motion getters

--- src/event/pointer.rs
+++ src/event/pointer.rs
@@ -1,5 +1,6 @@
 use crate::sys::{
-    libinput_event_get_pointer_event, libinput_event_pointer, libinput_event_pointer_get_base_event,
+    self, libinput_event_get_pointer_event, libinput_event_pointer,
+    libinput_event_pointer_get_base_event,
 };
 
 super::define_events!(
@@ -15,3 +16,21 @@ super::define_events!(
     ScrollFinger,
     ScrollContinuous,
 );
+
+impl PointerMotionAbsoluteEvent {
+    pub fn absolute_x(&self) -> f64 {
+        unsafe { sys::libinput_event_pointer_get_absolute_x(self.raw) }
+    }
+
+    pub fn absolute_x_transformed(&self, width: u32) -> f64 {
+        unsafe { sys::libinput_event_pointer_get_absolute_x_transformed(self.raw, width) }
+    }
+
+    pub fn absolute_y(&self) -> f64 {
+        unsafe { sys::libinput_event_pointer_get_absolute_y(self.raw) }
+    }
+
+    pub fn absolute_y_transformed(&self, width: u32) -> f64 {
+        unsafe { sys::libinput_event_pointer_get_absolute_y_transformed(self.raw, width) }
+    }
+}
