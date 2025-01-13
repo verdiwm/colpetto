aef7dac04397bb50841098940fa57018b8ded185
Matilde Morrone <contact@morrone.dev>
2025-01-13 18:15:04 +0100

    Implement tablet pad events

--- src/event/table_pad.rs
+++ src/event/table_pad.rs
@@ -1,8 +1,26 @@
+use crate::sys::{
+    libinput_event_get_tablet_pad_event, libinput_event_tablet_pad,
+    libinput_event_tablet_pad_get_base_event,
+};
+
+use super::define_events;
+
 #[derive(Debug)]
 pub enum TabletPadEvent {
-    Button,
-    Ring,
-    Strip,
-    Key,
-    Dial,
+    Button(TabletPadButtonEvent),
+    Ring(TabletPadRingEvent),
+    Strip(TabletPadStripEvent),
+    Key(TabletPadKeyEvent),
+    Dial(TabletPadDialEvent),
 }
+
+define_events!(
+    libinput_event_tablet_pad,
+    libinput_event_tablet_pad_get_base_event,
+    libinput_event_get_tablet_pad_event,
+    TabletPadButtonEvent,
+    TabletPadRingEvent,
+    TabletPadStripEvent,
+    TabletPadKeyEvent,
+    TabletPadDialEvent,
+);
