e360488c92c21f8f278af7e4b645e65058e32dc3
Matilde Morrone <contact@morrone.dev>
2025-01-13 17:43:00 +0100

    Make device events naming consistent

--- src/event/device.rs
+++ src/event/device.rs
@@ -7,13 +7,14 @@ use super::define_events;
 
 #[derive(Debug)]
 pub enum DeviceEvent {
-    Added(DeviceNotifyEvent),
-    Removed(DeviceNotifyEvent),
+    Added(DeviceAddedEvent),
+    Removed(DeviceRemovedEvent),
 }
 
 define_events!(
     libinput_event_device_notify,
     libinput_event_device_notify_get_base_event,
     libinput_event_get_device_notify_event,
-    DeviceNotifyEvent,
+    DeviceAddedEvent,
+    DeviceRemovedEvent,
 );
