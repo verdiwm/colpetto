5ffafc481a56297602e664d34d61f0dfe7c65c2a
Matilde Morrone <contact@morrone.dev>
2025-01-13 18:30:59 +0100

    Handle device events in print_keys example

--- examples/print_keys.rs
+++ examples/print_keys.rs
@@ -2,7 +2,7 @@ use std::{ffi::CString, path::Path};
 
 use anyhow::Result as AnyResult;
 use colpetto::{
-    event::{AsRawEvent, KeyboardEvent},
+    event::{AsRawEvent, DeviceEvent, KeyboardEvent},
     Event, Libinput,
 };
 use futures_util::TryStreamExt;
@@ -30,18 +30,34 @@ async fn main() -> AnyResult<()> {
     let mut stream = libinput.event_stream()?;
 
     while let Some(event) = stream.try_next().await? {
-        if let Event::Keyboard(KeyboardEvent::Key(event_key)) = event {
-            let device = event_key.device();
-            let device_name = device.name().to_string_lossy();
-            let udev_device = device.udev_device();
-            let device_path = udev_device.devnode().unwrap_or(Path::new("Unknown"));
-            let state = event_key.key_state();
-            let key = event_key.key();
+        match event {
+            Event::Keyboard(KeyboardEvent::Key(event_key)) => {
+                let device = event_key.device();
+                let device_name = device.name().to_string_lossy();
+                let udev_device = device.udev_device();
+                let device_path = udev_device.devnode().unwrap_or(Path::new("Unknown"));
+                let state = event_key.key_state();
+                let key = event_key.key();
 
-            println!(
-                "Key \"{key}\" {state} on \"{device_name}\" at node \"{device_path}\"",
-                device_path = device_path.display()
-            )
+                println!(
+                    "Key \"{key}\" {state} on \"{device_name}\" at node \"{device_path}\"",
+                    device_path = device_path.display()
+                )
+            }
+            Event::Device(DeviceEvent::Added(event)) => {
+                let device = event.device();
+                let name = device.name().to_string_lossy();
+
+                println!("Found new device: \"{name}\"")
+            }
+
+            Event::Device(DeviceEvent::Removed(event)) => {
+                let device = event.device();
+                let name = device.name().to_string_lossy();
+
+                println!("Lost device: \"{name}\"")
+            }
+            _ => (),
         }
     }
 
