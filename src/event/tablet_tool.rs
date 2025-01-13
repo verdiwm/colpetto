f0e612bc68fa681e21b7fedfd81cd8a7190eb327
Matilde Morrone <contact@morrone.dev>
2025-01-13 18:15:35 +0100

    Implement tablet tool events

--- src/event/tablet_tool.rs
+++ src/event/tablet_tool.rs
@@ -1,7 +1,24 @@
+use crate::sys::{
+    libinput_event_get_tablet_tool_event, libinput_event_tablet_tool,
+    libinput_event_tablet_tool_get_base_event,
+};
+
+use super::define_events;
+
 #[derive(Debug)]
 pub enum TabletToolEvent {
-    Axis,
-    Proximity,
-    Tip,
-    Button,
+    Axis(TabletToolAxisEvent),
+    Proximity(TabletToolProximityEvent),
+    Tip(TabletToolTipEvent),
+    Button(TabletToolButtonEvent),
 }
+
+define_events!(
+    libinput_event_tablet_tool,
+    libinput_event_tablet_tool_get_base_event,
+    libinput_event_get_tablet_tool_event,
+    TabletToolAxisEvent,
+    TabletToolProximityEvent,
+    TabletToolTipEvent,
+    TabletToolButtonEvent,
+);
