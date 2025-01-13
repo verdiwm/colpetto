31bc5d9507f532a704d6149b89aa80c03b2c37d9
Federico Maria Morrone <contact@morrone.dev>
2025-01-13 18:15:23 +0100

    Implement switch events

--- src/event/switch.rs
+++ src/event/switch.rs
@@ -1,4 +1,17 @@
+use crate::sys::{
+    libinput_event_get_switch_event, libinput_event_switch, libinput_event_switch_get_base_event,
+};
+
+use super::define_events;
+
 #[derive(Debug)]
 pub enum SwitchEvent {
-    Toggle,
+    Toggle(SwitchToggleEvent),
 }
+
+define_events!(
+    libinput_event_switch,
+    libinput_event_switch_get_base_event,
+    libinput_event_get_switch_event,
+    SwitchToggleEvent,
+);
