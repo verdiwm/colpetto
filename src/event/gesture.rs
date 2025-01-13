17b2afe02f57d6110eb8fe608373fd6f3cd8f204
Federico Maria Morrone <contact@morrone.dev>
2025-01-13 17:02:14 +0100

    Implement gesture events

--- src/event/gesture.rs
+++ src/event/gesture.rs
@@ -1,11 +1,31 @@
+use crate::sys::{
+    libinput_event_gesture, libinput_event_gesture_get_base_event, libinput_event_get_gesture_event,
+};
+
+use super::define_events;
+
 #[derive(Debug)]
 pub enum GestureEvent {
-    SwipeBegin,
-    SwipeUpdate,
-    SwipeEnd,
-    PinchBegin,
-    PinchUpdate,
-    PinchEnd,
-    HoldBegin,
-    HoldEnd,
+    SwipeBegin(GestureSwipeBeginEvent),
+    SwipeUpdate(GestureSwipeUpdateEvent),
+    SwipeEnd(GestureSwipeEndEvent),
+    PinchBegin(GesturePinchBeginEvent),
+    PinchUpdate(GesturePinchUpdateEvent),
+    PinchEnd(GesturePinchEndEvent),
+    HoldBegin(GestureHoldBeginEvent),
+    HoldEnd(GestureHoldEndEvent),
 }
+
+define_events!(
+    libinput_event_gesture,
+    libinput_event_gesture_get_base_event,
+    libinput_event_get_gesture_event,
+    GestureSwipeBeginEvent,
+    GestureSwipeUpdateEvent,
+    GestureSwipeEndEvent,
+    GesturePinchBeginEvent,
+    GesturePinchUpdateEvent,
+    GesturePinchEndEvent,
+    GestureHoldBeginEvent,
+    GestureHoldEndEvent,
+);
