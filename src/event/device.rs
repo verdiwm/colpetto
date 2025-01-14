82d4146bee5683b2c1fecf2596ece50fdbac159b
Matilde Morrone <contact@morrone.dev>
2025-01-14 20:10:54 +0100

    Fully document device module

--- src/event/device.rs
+++ src/event/device.rs
@@ -4,10 +4,15 @@ use crate::sys::{
 };
 
 super::define_events!(
+    /// An event notifying the caller of a device being added or removed.
     Device,
     libinput_event_device_notify,
     libinput_event_device_notify_get_base_event,
     libinput_event_get_device_notify_event,
+    /// Signals that a device has been added to the context.
+    /// The device will not be read until the next time the user calls libinput_dispatch() and data is available.
+    /// This allows setting up initial device configuration before any events are created.
     Added,
+    /// Signals that a device has been removed. No more events from the associated device will be in the queue or be queued after this event.
     Removed,
 );
