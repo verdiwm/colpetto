58b3d81faa8547435c7e96a9bf8ef71279693e51
Federico Maria Morrone <contact@morrone.dev>
2025-01-14 20:03:09 +0100

    Fully document keyboard module

--- src/event/keyboard.rs
+++ src/event/keyboard.rs
@@ -4,37 +4,55 @@ use crate::sys::{
 };
 
 super::define_events!(
+    /// A keyboard event representing a key press/release.
     Keyboard,
     libinput_event_keyboard,
     libinput_event_keyboard_get_base_event,
     libinput_event_get_keyboard_event,
+    /// Signals that a device has been removed. No more events from the associated device will be in the queue or be queued after this event.
     Key,
 );
 
 impl KeyboardKeyEvent {
+    /// The keycode that triggered this key event
     pub fn key(&self) -> u32 {
         unsafe { sys::libinput_event_keyboard_get_key(self.raw) }
     }
 
+    /// The state change of the key
     pub fn key_state(&self) -> KeyState {
         KeyState::from_raw(unsafe { sys::libinput_event_keyboard_get_key_state(self.raw) })
     }
 
+    /// The seat wide pressed key count for the key of this event
     pub fn seat_key_count(&self) -> u32 {
         unsafe { sys::libinput_event_keyboard_get_seat_key_count(self.raw) }
     }
 
+    /// The event time for this event
+    ///
+    /// # Note
+    ///
+    /// Timestamps may not always increase. See the libinput documentation for more details.
     pub fn time(&self) -> u32 {
         unsafe { sys::libinput_event_keyboard_get_time(self.raw) }
     }
 
+    /// The event time for this event in microseconds
+    ///
+    /// # Note
+    ///
+    /// Timestamps may not always increase. See the libinput documentation for more details.
     pub fn time_usec(&self) -> u64 {
         unsafe { sys::libinput_event_keyboard_get_time_usec(self.raw) }
     }
 }
 
+/// Logical state of a key. Note that the logical state may not represent the physical state of the key.
 pub enum KeyState {
+    /// Logical released state
     Released,
+    /// Logical pressed state
     Pressed,
 }
 
@@ -48,6 +66,7 @@ impl std::fmt::Display for KeyState {
 }
 
 impl KeyState {
+    /// Builds a new keystate from a raw libinput one
     pub const fn from_raw(raw: sys::libinput_key_state) -> Self {
         match raw {
             sys::libinput_key_state::LIBINPUT_KEY_STATE_RELEASED => Self::Released,
