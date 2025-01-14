67b440fc604ec54e1299afc2c1645e6382d740b9
Federico Maria Morrone <contact@morrone.dev>
2025-01-14 19:43:38 +0100

    Include readme as top level crate docs

--- src/lib.rs
+++ src/lib.rs
@@ -11,20 +11,7 @@
 #![warn(clippy::missing_errors_doc)]
 #![warn(clippy::missing_panics_doc)]
 #![warn(clippy::redundant_clone)]
-
-//! # Colpetto
-//! Colpetto is an async wrapper for libinput. It leverages the tokio runtime to
-//! offer a robust stream-based implementation for handling libinput events.
-//!
-//! Integrating with the async ecosystem allows for efficient polling of events,
-//! minimizing CPU consumption and maximizes performance in applications that
-//! require real-time event handling.
-//!
-//! You can find a simple example usage example [here](examples/print_keys.rs)
-//!
-//! Currently, Colpetto utilizes udev as the backend for device discovery and
-//! management. Future updates will include support for additional custom backends,
-//! allowing for greater flexibility.
+#![doc = include_str!("../README.md")]
 
 #[allow(missing_docs)]
 #[allow(nonstandard_style)]
