1e8f3e6d3c1546164d14a11017749458240fb5e3
Matilde Morrone <contact@morrone.dev>
2025-01-17 13:41:39 +0100

    Add support for multiple libinput versions

--- gen/src/main.rs
+++ gen/src/main.rs
@@ -1,8 +1,11 @@
-use anyhow::Result;
+use std::{fs, path::Path};
+
+use anyhow::{Context, Result};
+
+const VERSIONS: &[&str] = &["1.26", "1.27"];
 
 fn main() -> Result<()> {
-    let bindings = bindgen::Builder::default()
-        .header("/usr/include/libinput.h")
+    let base_builder = bindgen::builder()
         .allowlist_type("^libinput_.*$")
         .allowlist_function("^libinput_.*$")
         .use_core()
@@ -13,12 +16,40 @@ fn main() -> Result<()> {
         })
         .constified_enum("libinput_device_capability")
         .prepend_enum_name(false)
-        .constified_enum_module("libinput_event_type")
-        .generate()
-        .expect("Unable to generate bindings");
+        .constified_enum_module("libinput_event_type");
 
-    bindings
-        .write_to_file("../src/sys.rs")
-        .expect("Couldn't write bindings!");
+    if Path::new("../src/sys").exists() {
+        fs::remove_dir_all("../src/sys")?;
+    }
+
+    fs::create_dir_all("../src/sys")?;
+
+    let mut module = String::new();
+
+    for version in VERSIONS {
+        let bindings = base_builder
+            .clone()
+            .header(format!("src/headers/libinput-{version}.h"))
+            .generate()
+            .context("Failed to generate bindings")?;
+
+        let version = version.replace(".", "_");
+
+        bindings
+            .write_to_file(format!("../src/sys/sys_{version}.rs"))
+            .context("Couldn't write bindings")?;
+
+        module.push_str(&format!(
+            r#"
+        #[cfg(feature = "{version}")]
+        mod sys_{version};
+        #[cfg(feature = "{version}")]
+        pub use sys_{version}::*;
+        "#
+        ));
+    }
+
+    fs::write("../src/sys/mod.rs", &module)?;
+
     Ok(())
 }
