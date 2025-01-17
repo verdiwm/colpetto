2b870d69b4a55f5791c4ea4f67c2c6ab61cd2167
Matilde Morrone <contact@morrone.dev>
2025-01-17 13:55:37 +0100

    Add support for libinput 1.22

--- src/sys/mod.rs
+++ src/sys/mod.rs
@@ -1,26 +1,30 @@
 
-        #[cfg(feature = "1_23")]
-        mod sys_1_23;
-        #[cfg(feature = "1_23")]
-        pub use sys_1_23::*;
-        
-        #[cfg(feature = "1_24")]
-        mod sys_1_24;
-        #[cfg(feature = "1_24")]
-        pub use sys_1_24::*;
-        
-        #[cfg(feature = "1_25")]
-        mod sys_1_25;
-        #[cfg(feature = "1_25")]
-        pub use sys_1_25::*;
-        
-        #[cfg(feature = "1_26")]
-        mod sys_1_26;
-        #[cfg(feature = "1_26")]
-        pub use sys_1_26::*;
-        
-        #[cfg(feature = "1_27")]
-        mod sys_1_27;
-        #[cfg(feature = "1_27")]
-        pub use sys_1_27::*;
-        
+#[cfg(feature = "1_22")]
+mod sys_1_22;
+#[cfg(feature = "1_22")]
+pub use sys_1_22::*;
+
+#[cfg(feature = "1_23")]
+mod sys_1_23;
+#[cfg(feature = "1_23")]
+pub use sys_1_23::*;
+
+#[cfg(feature = "1_24")]
+mod sys_1_24;
+#[cfg(feature = "1_24")]
+pub use sys_1_24::*;
+
+#[cfg(feature = "1_25")]
+mod sys_1_25;
+#[cfg(feature = "1_25")]
+pub use sys_1_25::*;
+
+#[cfg(feature = "1_26")]
+mod sys_1_26;
+#[cfg(feature = "1_26")]
+pub use sys_1_26::*;
+
+#[cfg(feature = "1_27")]
+mod sys_1_27;
+#[cfg(feature = "1_27")]
+pub use sys_1_27::*;
