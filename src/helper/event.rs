62326beac8c4d9b7ef8dee3441ce7dbb4f0c45cd
Matilde Morrone <contact@morrone.dev>
2025-02-28 21:19:20 +0100

    Better map keyboard event

--- src/helper/event.rs
+++ src/helper/event.rs
@@ -8,22 +8,31 @@ pub struct Event {
 }
 
 #[derive(Debug)]
+#[non_exhaustive]
 pub enum EventType {
-    Keyboard {
+    Keyboard(KeyboardEvent),
+    Unknown,
+}
+
+#[derive(Debug)]
+#[non_exhaustive]
+pub enum KeyboardEvent {
+    Key {
         key: u32,
         state: libinput_event::KeyState,
+        time: u64,
     },
-    Unknown,
 }
 
 impl From<&crate::Event> for EventType {
     fn from(value: &crate::Event) -> Self {
         match value {
             crate::Event::Keyboard(libinput_event::KeyboardEvent::Key(event)) => {
-                EventType::Keyboard {
+                EventType::Keyboard(KeyboardEvent::Key {
                     key: event.key(),
                     state: event.key_state(),
-                }
+                    time: event.time_usec(),
+                })
             }
             _ => EventType::Unknown,
         }
